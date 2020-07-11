3f37e695f3b3fc058599ddad184bfd17677917cc
abdoubouss <63803114+abdoubouss@users.noreply.github.com>
2020-07-11 22:24:39 +0200

    first commit

---
+++ ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -0,0 +1,16 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int myValue=10000;
+	int myMinIntValue=Integer.MIN_VALUE;
+	int myMaxValue=Integer.MAX_VALUE;
+    //System.out.println("Integer Minimum Value = ", + myMinIntValue);
+    //System.out.println("Integer Maximum Value = ", + myMaxValue);
+     byte  myMinByteValue=Byte.MIN_VALUE;
+     byte  myMaxByteValue=Byte.MAX_VALUE;
+     System.out.println("Byte Minimun Value= ",+ myMinByteValue);
+     System.out.println("Byte Maximun Value= ", + myMaxByteValue);
+        }
+}
